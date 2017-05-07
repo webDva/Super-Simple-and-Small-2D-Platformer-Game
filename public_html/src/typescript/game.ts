537f7870ee18fffd99a9617d61fa2d04708ce2cc
jermaine <jaylarcane@gmail.com>
2017-05-06 20:05:42 -0400

    added pantsuweb logo as placeholder and icon

--- public_html/src/typescript/game.ts
+++ public_html/src/typescript/game.ts
@@ -6,6 +6,7 @@ class SimpleGame {
     
     game: Phaser.Game;
     block: Phaser.Sprite;
+    logo: Phaser.Sprite;
     
     constructor() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser", {preload: this.preload, create: this.create, update: this.update, render: this.render});
@@ -13,13 +14,15 @@ class SimpleGame {
     
     preload() {
         this.game.load.image('block', 'assets/placeholder.png');
+        this.game.load.image('logo', 'assets/pantsuweb2.png');
     }
     
     create() {
         this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; // will set it to RESIZE later for responsiveness
         
-        this.block = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'block');
-        this.block.anchor.setTo(0.5, 0.5);
+        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        this.logo.scale.setTo(0.2, 0.2);
+        this.logo.anchor.setTo(0.5, 0.5);
     }
     
     update() {
